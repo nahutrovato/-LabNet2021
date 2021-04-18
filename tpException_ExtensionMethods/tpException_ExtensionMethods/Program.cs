b26adc3ba3a5b432e3284b3f8aa42212b21551a0
nahutrovato <trovatonahuel@gmail.com>
2021-04-18 20:32:21 -0300

    Terminado ejercicio numero 2

--- tpException_ExtensionMethods/tpException_ExtensionMethods/Program.cs
+++ tpException_ExtensionMethods/tpException_ExtensionMethods/Program.cs
@@ -10,19 +10,23 @@ namespace tpException_ExtensionMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese un numero para dividir");
-            string ingresoDividendo = Console.ReadLine();
-            int dividendo = int.Parse(ingresoDividendo);
-            Dividir(dividendo);
+          
+            Console.WriteLine("Ejercicio numero 1: Se intentara dividir el dividendo ingresado por 0:");
+            DividiendoPorCero();
+            Console.WriteLine("Ejercicio numero 2: Se intentara dividir el dividendo por el divisor ingresado");
+            FnDividir();
             Console.Read();
         }
 
-        public static void Dividir(int dividendo)
+        public static void DividiendoPorCero()
         {
+            Console.WriteLine("Ingrese un numero para dividir");
+            string ingresoDividendo = Console.ReadLine();
+            int dividendoIngresado = int.Parse(ingresoDividendo);
             try
             {
-                int resultado = dividendo / 0;
-                Console.WriteLine(resultado);
+                int resultadoEjercicio1 = dividendoIngresado / 0;
+                Console.WriteLine(resultadoEjercicio1);
             }
             catch (Exception)
             {
@@ -30,7 +34,34 @@ namespace tpException_ExtensionMethods
             }
             finally
             {
-                Console.WriteLine("Se termino la excepcion");
+                Console.WriteLine("Se termino la excepcion del ejercicio 1");
+            }
+        }
+        
+        public static void FnDividir()
+        {
+           try
+            {
+                Console.WriteLine("Ingrese un numero para dividir");
+                string ingresoDividendo = Console.ReadLine();
+                int dividendoIngresado = int.Parse(ingresoDividendo);
+                Console.WriteLine("Ingrese un numero divisor");
+                string ingresoDivisor = Console.ReadLine();
+                int divisorIngresado = int.Parse(ingresoDivisor);
+                int resultadoEjercicio2 = dividendoIngresado / divisorIngresado;
+                Console.WriteLine($"El resultado de {dividendoIngresado} dividido {divisorIngresado} es: {resultadoEjercicio2}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error en el ingreso de formato, ingreso una letra");   
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("No se puede dividir por cero");
+            }
+            finally
+            {
+                Console.WriteLine("Se termino la excepcion del ejercicio 2");
             }
         }
     }
