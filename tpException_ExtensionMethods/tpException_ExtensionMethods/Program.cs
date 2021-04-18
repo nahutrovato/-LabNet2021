c9b6a5a68b5b109e71cd9d908d4a76d58b9acf17
nahutrovato <trovatonahuel@gmail.com>
2021-04-18 18:43:04 -0300

    Ejercicio numero 1

--- tpException_ExtensionMethods/tpException_ExtensionMethods/Program.cs
+++ tpException_ExtensionMethods/tpException_ExtensionMethods/Program.cs
@@ -10,7 +10,28 @@ namespace tpException_ExtensionMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Exitos");
+            Console.WriteLine("Ingrese un numero para dividir");
+            string ingresoDividendo = Console.ReadLine();
+            int dividendo = int.Parse(ingresoDividendo);
+            Dividir(dividendo);
+            Console.Read();
+        }
+
+        public static void Dividir(int dividendo)
+        {
+            try
+            {
+                int resultado = dividendo / 0;
+                Console.WriteLine(resultado);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se puede dividir por cero");
+            }
+            finally
+            {
+                Console.WriteLine("Se termino la excepcion");
+            }
         }
     }
 }
