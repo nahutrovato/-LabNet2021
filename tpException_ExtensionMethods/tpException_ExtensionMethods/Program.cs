002f79abbe03a20a1c80e02454a225a408919040
nahutrovato <trovatonahuel@gmail.com>
2021-04-18 18:39:16 -0300

    Probando subida tp 2

---
+++ tpException_ExtensionMethods/tpException_ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpException_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exitos");
+        }
+    }
+}
