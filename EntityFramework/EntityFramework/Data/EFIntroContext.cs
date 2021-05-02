6f2fa99d8600d15f1876a5db044ee621678778d5
nahutrovato <trovatonahuel@gmail.com>
2021-05-02 18:11:27 -0300

    Se crea Categories

---
+++ EntityFramework/EntityFramework/Data/EFIntroContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFramework.Models;
+
+namespace EntityFramework.Data
+{
+    public class EFIntroContext : DbContext
+    {
+        public EFIntroContext (DbContextOptions<EFIntroContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityFramework.Models.Categories> Categories { get; set; }
+    }
+}
---
+++ EntityFramework/EntityFramework/Models/Categories.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace EntityFramework.Models
+{
+    public class Categories
+    {
+
+        [Key]
+        public int CategoryID{ get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
