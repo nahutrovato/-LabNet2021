50a912714871787f58095091c5a4147a4ea0b993
nahutrovato <trovatonahuel@gmail.com>
2021-05-04 22:17:16 -0300

    Se validan datos

--- formtp/js/main.js
+++ formtp/js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-    $('#submit').on('click', validarDatos);
     
-    
-
-
-    function validarDatos(){
-          
-    }
+    $('#botonSubmit').on('click', function(event){
+          event.preventDefault();
+      
+          var nombre = $('#nombreFormulario').val();
+          var apellido = $('#apellidoFormulario').val();
+          var edad = $('#edadFormulario').val();
+          var edad = $('#edadFormulario').val();
+          if(nombre == ""){
+              alert('No ingreso el nombre');
+          }else if( apellido == ""){
+            alert('No ingreso el apellido');
+          }else if( empresa == ""){
+            alert('No ingreso la empresa');
+          }
+    });
 });
