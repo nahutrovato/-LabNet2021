f16f9c52a85582dc13be6e38ab11e1f5841b6cba
nahutrovato <trovatonahuel@gmail.com>
2021-05-04 22:38:50 -0300

    Finalizacion del proyecto

--- formtp/js/main.js
+++ formtp/js/main.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
     
     $('#botonSubmit').on('click', function(event){
-          event.preventDefault();
+         event.preventDefault();
       
           var nombre = $('#nombreFormulario').val();
           var apellido = $('#apellidoFormulario').val();
           var edad = $('#edadFormulario').val();
-          var edad = $('#edadFormulario').val();
+          var genero = $("input[type = 'radio']:checked");
+          var empresa = $('#empresaFormulario').val();
+          var mensajeError = $('.mensajeError')
           if(nombre == ""){
-              alert('No ingreso el nombre');
+              mensajeError.css('display','flex').css('color','red');
+              mensajeError.text('No ingreso el nombre');
           }else if( apellido == ""){
-            alert('No ingreso el apellido');
+            mensajeError.css('display', 'flex').css('color','red');;
+              mensajeError.text('No ingreso el apellido');
+          }else if(genero.length == 0){
+            mensajeError.css('display', 'flex').css('color','red');;
+              mensajeError.text('No ingreso el genero');
           }else if( empresa == ""){
-            alert('No ingreso la empresa');
+            mensajeError.css('display', 'flex').css('color','red');;
+              mensajeError.text('No ingreso la empresa');
           }
+         
     });
 });
